e0f2e60c9c09949c6955fa620b11bd50e5169f9c
laxeder <laxederbr@gmail.com>
2023-12-10 17:02:06 -0300

    Fix script path

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Obtém o caminho do script
+script_path=$(realpath "$0")
+
+# Obtém o diretório do script
+script_directory=$(dirname "$script_path")
+
+# Abre o diretório do script
+cd "${script_directory}"
+
 # Inicia o bot
 sh npm start
